1b71845d2f990d9ec1bffe57a3db92a98f15a5bb
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-03-26 17:34:08 +0300

    places save edit photo deletion and primary selection

--- client/src/pages/PlacesFormPage.jsx
+++ client/src/pages/PlacesFormPage.jsx
@@ -61,14 +61,25 @@ export default function PlacesFormPage() {
         );
     }
 
-    async function addNewPlace(ev) {
+    async function savePlace(ev) {
         ev.preventDefault();
-        await axios.post('/places', {
+        const placeData = {
             title, address, addedPhotos,
             description, perks, extraInfo,
             checkIn, checkOut, maxGuests
-        });
-        setRedirect('true')
+        };
+        if (id) {
+            // update
+            await axios.put('/places', {
+                id,
+                ...placeData
+            });
+        } else {
+            // new place
+            await axios.post('/places', placeData);
+        }
+        setRedirect('true');
+
     }
 
     if (redirect) {
@@ -79,7 +90,7 @@ export default function PlacesFormPage() {
         <>
             <div>
                 <AccountNavigation />
-                <form onSubmit={addNewPlace}>
+                <form onSubmit={savePlace}>
                     {preInput('Title', 'Title for your place. Should be short and catchy as in advertisement')}
                     <input type="text" value={title} onChange={ev => setTitle(ev.target.value)} placeholder="title, for example: My lovely apt." />
                     {preInput('Address', 'Address to this place')}
