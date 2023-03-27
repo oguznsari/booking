51c0096a06400d0c679815011ae9103f783a8eef
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-03-27 13:41:06 +0300

    Single place page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import { UserContextProvider } from './UserContext.jsx'
 import ProfilePage from './pages/ProfilePage.jsx'
 import PlacesPage from './pages/PlacesPage.jsx'
 import PlacesFormPage from './pages/PlacesFormPage.jsx'
+import PlacePage from './pages/PlacePage.jsx'
 
 axios.defaults.baseURL = 'http://localhost:4000';
 axios.defaults.withCredentials = true;
@@ -24,6 +25,7 @@ function App() {
           <Route path='/account/places' element={<PlacesPage />} />
           <Route path='/account/places/new' element={<PlacesFormPage />} />
           <Route path='/account/places/:id' element={<PlacesFormPage />} />
+          <Route path='/place/:id' element={<PlacePage />} />
         </Route>
       </Routes>
     </UserContextProvider>
