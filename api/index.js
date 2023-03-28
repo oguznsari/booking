df8841d43c0278c5af9ba7fd7b13b10f96b83956
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-03-28 17:45:21 +0300

    API documentation swagger ui

--- api/index.js
+++ api/index.js
@@ -8,6 +8,11 @@ const mainRouter = require('./routes/main')
 // connectDB
 const connectDB = require('./db/connect');
 
+// swagger ui
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDoc = YAML.load('./swagger.yaml');
+
 app.use(cors({
     credentials: true,
     origin: 'http://localhost:5173'
@@ -17,6 +22,7 @@ app.use(cookieParser());                                        // cookie parser
 
 app.use('/uploads', express.static(__dirname + '/uploads'))
 app.use('/', mainRouter);
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDoc));
 
 const port = process.env.PORT || 4000;
 const start = async () => {
--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -26,21 +26,33 @@ export default function Header() {
                 </button>
             </div>
 
-            <Link to={user ? '/account' : '/login'} className='flex items-center gap-3 border border-gray-300 rounded-full py-2 px-4'>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-                </svg>
-                <div className='bg-gray-500 text-white rounded-full border border-gray-500 overfill-hidden'>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 relative top-1">
-                        <path fillRule="evenodd" d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z" clipRule="evenodd" />
+            <div>
+                <Link to={user ? '/account' : '/login'} className='flex items-center gap-3 border border-gray-300 rounded-full py-2 px-4'>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                     </svg>
-                </div>
-                {!!user && (
-                    <div>
-                        {user.name}
+                    <div className='bg-gray-500 text-white rounded-full border border-gray-500 overfill-hidden'>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 relative top-1">
+                            <path fillRule="evenodd" d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z" clipRule="evenodd" />
+                        </svg>
                     </div>
-                )}
-            </Link>
+                    {!!user && (
+                        <div>
+                            {user.name}
+                        </div>
+                    )}
+                </Link>
+                <a className='bg-gray-200 py-2 px-4 rounded-full flex gap-2 items-center shadow shadow-black fixed bottom-5 right-5' href='http://localhost:4000/api-docs/' target={'_blank'}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 15a4.5 4.5 0 004.5 4.5H18a3.75 3.75 0 001.332-7.257 3 3 0 00-3.758-3.848 5.25 5.25 0 00-10.233 2.33A4.502 4.502 0 002.25 15z" />
+                    </svg>
+                    <span className='text-xs'>
+                        Api Doc
+                    </span>
+                </a>
+            </div>
+
+
         </header>
     );
 }
