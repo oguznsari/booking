c735232dec6ed9132539d670c64c98290f8f1d57
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-03-28 13:07:44 +0300

    code refactor, main router, added controllers for specific tasks-2

--- api/controllers/image.js
+++ api/controllers/image.js
@@ -6,7 +6,6 @@ const uploadWithLink = async (req, res) => {
     const { link } = req.body;
     const newName = 'photo' + Date.now() + '.jpg'
     const newPath = path.join(__dirname, '..', 'uploads', newName);
-    console.log({ newPath });
     await imageDownloader.image({
         url: link,
         dest: newPath,
@@ -16,18 +15,14 @@ const uploadWithLink = async (req, res) => {
 
 const uploadImageFile = (req, res) => {
     const uploadedFiles = [];
-    console.log(Object.entries(req.files));
     for (const [key, value] of Object.entries(req.files)) {
         const { path: filePath, originalname } = value;
         const parts = originalname.split('.');
         const ext = parts[parts.length - 1];
         const newPath = filePath + '.' + ext;
-        console.log({ filePath })
-        console.log({ newPath })
         fs.renameSync(filePath, newPath);
         uploadedFiles.push(newPath.replace('uploads\\', ''));
     }
-    console.log({ uploadedFiles })
     res.json(uploadedFiles)
 }
 
